a16803070d8d9afb94bee4e1fce454f507f224a1
Fong, Sean (H&B, Herston - RBWH) <Sean.Fong@csiro.au>
2021-12-01 13:07:22 +1030

    built simple dashboard

--- dashapp/event/generator.py
+++ dashapp/event/generator.py
@@ -15,7 +15,7 @@ class Generator:
         start_time = time.time()
         print('Start time:', start_time)
         for event in events:
-            s.enter(event['timestamp'], 1, send_event, argument=(url, event, start_time,))
+            s.enter(event['elapsed'], 1, send_event, argument=(url, event, start_time,))
         s.run()
         print('End time:', time.time())
 
@@ -23,33 +23,29 @@ class Generator:
         with open('/home/yeexianfong/real-time-fhir/dashapp/input/DiagnosticReport.ndjson', 'r', encoding='latin-1',) as in_file:
             events = generate_events(in_file)
 
-        return normalize_timestamps(sorted(events, key=lambda d: d['timestamp']), self.duration)
+        return normalize_elapsed(sorted(events, key=lambda d: d['elapsed']), self.duration)
 
 # helper functions
 def generate_events(infile):
-        return [{'resource': json.loads(line), 'timestamp': datetime.fromisoformat(json.loads(line)['effectiveDateTime']).timestamp()} for line in infile]
+        return [{'resource': json.loads(line), 'elapsed': datetime.fromisoformat(json.loads(line)['effectiveDateTime']).timestamp()} for line in infile]
 
-def normalize_timestamps(events, duration):
-    range = events[-1]['timestamp'] - events[0]['timestamp']
-    min = events[0]['timestamp']
+def normalize_elapsed(events, duration):
+    range = events[-1]['elapsed'] - events[0]['elapsed']
+    min = events[0]['elapsed']
     for event in events:
-        event['timestamp'] = (event['timestamp']-min)/range * duration
+        event['elapsed'] = (event['elapsed']-min)/range * duration
     return events
 
 def send_event(url, event, t):
-    #event = {'timestamp': timestamp, 'resource': 'somevalues-successful'}
-    #payload = {'key1': 'value1', 'key2': 'value2'}
-
     r = requests.post(url, json=event)
     print(time.time()-t, r.status_code)
 
-
 if __name__ == '__main__':
-    url='http://localhost:5000/dash'
+    url='http://localhost:5000/generate_events'
 
-    gen = Generator(30)
+    gen = Generator(300)
     events = gen.read_EHR_data()
     for i in events:
-        print(i['timestamp'], i['resource']['id'])
+        print(i['elapsed'], i['resource']['id'])
         
     gen.send_events(events, url)
